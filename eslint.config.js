57daa0bb9f10bdba8326569c8b2f58053ebbe1fe
eynoguer <edilson.noguera@bancolombia.com.co>
2025-01-28 20:41:13 -0500

    Refactor archivos de configuración y mejorar la estructura del proyecto; agregar configuración de Prettier y optimizar archivos de Tailwind y TypeScript.

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,16 @@
-
-import js from '@eslint/js'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"], // Asegúrate de incluir esto para que se aplique a archivos TS/TSX
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      parserOptions: {
+        project: ['./tsconfig.json'], //  <- Ruta a tu tsconfig.json
+        tsconfigRootDir: import.meta.dirname, // <-  Directorio raíz para tsconfig.json
+      },
     },
   },
-)
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+);
--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
-import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
 
 export default {
-  plugins: [
-    tailwindcss('./tailwind.config.js'),
-    autoprefixer,
-  ],
-};
+  plugins: [tailwindcss('./tailwind.config.js'), autoprefixer],
+}
